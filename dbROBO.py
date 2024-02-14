1eed4277e5397d27c1a75f517462bfe90af6215f
MamaMia07 <kokoszkam@gazeta.pl>
2024-02-14 11:58:57 +0100

    db

--- dbROBO.py
+++ dbROBO.py
@@ -2,7 +2,7 @@
 
 import psycopg2, time, datetime
 from configparser import ConfigParser
-
+from psycopg2 import extras
 
 def config(filename = 'database.ini', section = 'postgresql'):
     parser = ConfigParser()
@@ -31,7 +31,11 @@ def connect(func):
 
             # Create a cursor to perform database operations
             cursor = connection.cursor()
-            #cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            # kursor - wynik zapytania jest slownikiem
+            #cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
+
+
             #print("PostgreSQL server information")
            # print(connection.get_dsn_parameters(),'\n')
 
@@ -68,48 +72,60 @@ def connect(func):
 
 
 
-#!!!!!!!!!!!  "Dictionary-like cursor", part of psycopg2.extras !!!!!!!!!!!!!
 
 
+##  Nowy użytkownik - do bazy
+##@connect
+##def insert_new_account(username, name, passw): #(cursor, connection):
+##    tab = 'account'
+##    postgres_query = f'''INSERT INTO {tab} (username, password, name, created_on)
+##VALUES (%s, %s, %s, %s) RETURNING user_id'''          
+##    values = (username, name, passw, datetime.datetime.now())              
+##    return postgres_query, values
+##
+###print("Inserted user_id: ", insert_new_account('lala', 'olla', 'haselko'), "\n")
 
-@connect
-def insert_new_account(username, name, passw): #(cursor, connection):
-    tab = 'account'
-    postgres_query = f'''INSERT INTO {tab} (username, password, name, created_on)
-VALUES (%s, %s, %s, %s) RETURNING user_id'''          
-    values = (username, name, passw, datetime.datetime.now())              
-    return postgres_query, values
 
-
-#print("Inserted user_id: ", insert_new_account('lala', 'olla', 'haselko'), "\n")
-
-
+##  lista uzytkowników z bazy
 @connect
 def select_users():
-    tab = 'account'
-    postgres_query = f"SELECT username FROM {tab}"
+    postgres_query = f"SELECT username FROM account"
     values = None
     return postgres_query, values
 
-
 users = select_users()
+
 print("Selected names: ", users)
-#print("W bazie istnieje użytkownik 'lala'? ",any(user[0] =='lala' for user in users))
+
+##  czy dany uzytkownik istnieje w bazie?
+is_user_in_bd = any(user[0] =='uuu' for user in users)
+print("W bazie istnieje użytkownik 'uuu'? ",any(user[0] =='uuu' for user in users))
+
+
+###         wydruk słownika otrzymanego jako rezultat z bazy, po zapytaniu:
+###         cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
+##for row in users:
+##    print("--------")
+##    for key, value in row.items():
+##        print(f"{key}: {value}")
 
 
 
-@connect
-def new_message(sender, recipient, content):
-    tab = 'message'
-    query = f'''INSERT INTO {tab} (sender, recipient, content, created_time)
-VALUES (%s, %s, %s, %s) RETURNING message_id'''          
-    values = (sender, recipient, content, datetime.datetime.now())              
-    return query, values
+####  OK !!
+####  Nowa wiadomosc do bazy danych
+##@connect
+##def new_message(sender, recipient, content):
+##    tab = 'message'
+##    query = f'''INSERT INTO {tab} (sender, recipient, content, created_time)
+##VALUES (%s, %s, %s, %s) RETURNING message_id'''          
+##    values = (sender, recipient, content, datetime.datetime.now())              
+##    return query, values
+##
+##print("Inserted message_id: ", new_message('nada', 'odbioe', 'takka se wiadomosc'), "\n")
+##
 
-#print("Inserted message_id: ", new_message('from', 'to', '3. message new message'), "\n")
 
-
-
+### wybierz wiadomosc DO zadanego użytkownika
 @connect
 def select_messages_to(name):
     query = f"SELECT (message_id, sender, recipient, content, created_time) FROM message WHERE recipient = %s "
@@ -117,24 +133,46 @@ def select_messages_to(name):
     return query, values
 
 aa = select_messages_to('to')
-for row in aa:
-    print(type(row))
-    print("code: {}".format(row[0])) 
-    print("name: {}".format(row[0][2])) 
-    print("concatenated_column: {}".format(row[0][3])) 
-#print(select_messages_to('to'))
+print(aa)
+
+### zamiana wyniku zapytania o wiadomosci do bazy na format słownika
+def databese_result_to_dict(db_result):
+    dictionary = {}
+    for row in db_result:
+        row = row[0][1:-1].split(",")
+        dictionary[row[0]] = {'sender' : row[1],
+                            'recipient' : row[2],
+                            'content' : row[3].strip('"'),
+                            'datetime' : row[4].strip('"'),
+                            }
+    return dictionary
 
 
+bb = databese_result_to_dict(aa)
+print(bb)
 
+
+### ustaw status wiadomości o id = msg_id na "przeczytana" 
 @connect
 def mark_msg_read(msg_id):
     query = f"UPDATE message SET read = True WHERE message_id = %s "
     values = (msg_id, )
     return query, values
 
-print(mark_msg_read(1))
+print(mark_msg_read(18))
 
-    
+   
+
+###   !!!!! ZMIANA hasła
+
+@connect
+def change_pswrd(pswrd, username):
+    query = f"UPDATE account SET password = %s WHERE username = %s "
+    values = (pswrd, username,)
+    return query, values
+
+print(change_pswrd('Nowehaselko', 'lala'))
+
 ## # polaczenie z baza i wykonanie operacji.
 ##def connect():
 ##    try:
